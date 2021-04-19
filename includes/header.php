17486b6ee70a5fdc7d59a8b0999e7253ecdad87d
Ciaran <d00230698@student.dkit.ie>
2021-04-19 22:04:43 +0100

    Added contact form

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,9 @@
 <head>
 <title>My PHP CRUD App</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
+</style>	
+<!-- a helper script for validating the form-->
+<script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
 </head>
 
 <!-- the body section -->
