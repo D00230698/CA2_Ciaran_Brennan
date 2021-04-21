672e34e7065cd19e78694617f075949549ac9cf6
Ciaran <d00230698@student.dkit.ie>
2021-04-22 00:27:10 +0100

    css on menu

--- includes/header.php
+++ includes/header.php
@@ -10,10 +10,10 @@
 <!-- the body section -->
 <body>
 <header><h1>The Olympics</h1>
-<div id="mySidenav" class="sidenav">
-  <a href="#" id="about">About</a>
-  <a href="#" id="blog">Blog</a>
-  <a href="#" id="projects">Projects</a>
-  <a href="#" id="contact">Contact</a>
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="add_record.php">Add Player</a>
+  <a href="contact.php">Contact</a>
+  <a href="#about">About</a>
 </div>
 </header>
